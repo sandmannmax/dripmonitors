6d6be2082e52ad4285ed537d51397cc6df50d008
sandmannmax <m1ng4@protonmail.com>
2021-01-30 13:21:12 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <h1 class="text-center">
+        <img src="logo.png" width="70"/>
+        <span id="title">Lazy Monitor</span>
+      </h1>
+      <div class="text-center margin">
+        <p>Get your customizable Sneaker-Monitor for your Cook-Group.</p>
+        <img class="embeds" src="Embeds.png"/>
+      </div>     
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+@Component
+export default class Home extends Vue {
+}
+</script>
+
+<style scoped>
+  .margin {
+    margin-top: 30px;
+  }
+
+  .embeds {
+    width: 500px;
+    max-width: 90vw; 
+    margin: 50px auto;
+  }
+
+  #title {
+    margin-left: 20px;
+    font-weight: 500;
+  }
+
+  p {
+    font-size: 1.4em;
+  }
+</style>
