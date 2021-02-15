d1e0af3b7ba14a1a659e327a0663a5a0a901cbca
sandmannmax <m1ng4@protonmail.com>
2021-02-15 18:04:40 +0100

    changed GetMonitors for Monitor, because forgot monitorsources

--- src/models/MonitorModel.ts
+++ src/models/MonitorModel.ts
@@ -1,5 +1,6 @@
 import { DatabaseProvider } from '../provider/DatabaseProvider';
 import { Monitor } from '../types/Monitor';
+import { Monitorsource } from '../types/Montiorsource';
 import { Product } from '../types/Product';
 
 const dbProvider = DatabaseProvider.getInstance();
@@ -8,22 +9,32 @@ export class MonitorModel {
 
   public static GetMonitors = async function ({ product, monitorpageId }: { product: Product, monitorpageId: string }): Promise<Array<Monitor>> {
     let monitors: Array<Monitor> = [];
+    let monitorsources: Array<Monitorsource> = [];
 
-    let result = await dbProvider.Scan('lsb.monitors', "#a = :all", { ":all": true }, { '#a': 'all' });
+    let result = await dbProvider.Scan('lsb.monitorsources', "#a = :all", { ":all": true }, { '#a': 'all' });
     if (result.Items != null)
-      monitors.push(...(result.Items as Array<Monitor>));
+      monitorsources.push(...(result.Items as Array<Monitorsource>));
 
     if (product) {
-      result = await dbProvider.Scan('lsb.monitors', "monitorpageId = :monitorpageId", { ":monitorpageId": monitorpageId });
+      result = await dbProvider.Scan('lsb.monitorsources', "monitorpageId = :monitorpageId", { ":monitorpageId": monitorpageId });
       if (result.Items != null)
-        monitors.push(...(result.Items as Array<Monitor>));
+      monitorsources.push(...(result.Items as Array<Monitorsource>));
     }
 
     if (product) {
-      result = await dbProvider.Scan('lsb.monitors', "productId = :productId", { ":productId": product.id });
+      result = await dbProvider.Scan('lsb.monitorsources', "productId = :productId", { ":productId": product.id });
       if (result.Items != null)
-        monitors.push(...(result.Items as Array<Monitor>));
+      monitorsources.push(...(result.Items as Array<Monitorsource>));
     }
+
+    monitorsources.forEach(async (monitorsource) => {
+      let index = monitors.findIndex(monitor => monitor.id == monitorsource.id);
+
+      if (index === -1) {
+        let result = await dbProvider.Get('lsb.monitors', { id: monitorsource.monitorId });
+        monitors.push(result.Item as Monitor);
+      }
+    });
 
     return monitors;
   }
