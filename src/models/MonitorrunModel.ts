5685110b2df184cab9e457906b6ef30482111153
sandmannmax <m1ng4@protonmail.com>
2021-02-24 20:31:01 +0100

    more debug

--- src/models/MonitorrunModel.ts
+++ src/models/MonitorrunModel.ts
@@ -1,3 +1,4 @@
+import { logger } from '../logger';
 import { DatabaseProvider } from '../provider/DatabaseProvider';
 import { Monitorrun } from '../types/Monitorrun';
 
@@ -5,11 +6,14 @@ const dbProvider = DatabaseProvider.getInstance();
 
 export class MonitorrunModel {
   public static AddMonitorrun = async function ({ monitorrun }: { monitorrun: Monitorrun }) {
+    logger.info('AddMonitorrun ' + JSON.stringify(monitorrun));
     await dbProvider.Insert('lsb.monitorruns', monitorrun.ToDBObject());
   }
 
   public static IdUnused = async function ({ id }: { id: string }): Promise<boolean> {
+    logger.info('IsUnused ' + id)
     let result = await this.dbProvider.Get('lsb.monitorruns', { id });
+    logger.info('... ' + JSON.stringify(result));
     return result.Item == null;
   }
 }
